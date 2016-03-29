03090b2a5d96a014a7670217373726811b4d19d3
averyirin <averyirin@gmail.com>
2016-03-29 14:27:12 -0400

    Added cart controller actions

--- _TEST/cart/cart_controller.php
+++ _TEST/cart/cart_controller.php
@@ -14,4 +14,28 @@ else if($action == "end_cart"){
         unset($_SESSION['cart']);
     }
 }
+//view cart
+else if($action == "view_cart") {
+    echo "Include shop_cart.php with queries done";
+}
+
+//add product to cart
+else if($action == "add_product") {
+    echo "Add product to cart";
+}
+
+//remove product to cart
+else if($action == "remove_product") {
+    echo "Remove product from cart";
+}
+
+//update product quantity in cart
+else if($action == "update_quantity") {
+    echo "Update product quantity in cart";
+}
+
+//clear the cart
+else if($action == "empty_cart") {
+    echo "Cart is now empty";
+}
 ?>
